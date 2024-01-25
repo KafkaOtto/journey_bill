814a5dfbca1c50c441f9662deab5993b67b358ad
guozhinuan <guozhinuan@bigo.sg>
2024-01-25 14:56:34 +0100

    upgrade docker image version

--- bill-backend/docker.sh
+++ bill-backend/docker.sh
@@ -1,1 +1,1 @@
-mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ottokafka2/bill-backend:0.0.4
+mvn spring-boot:build-image -Dspring-boot.build-image.imageName=ottokafka2/bill-backend:0.0.5
