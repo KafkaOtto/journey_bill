dd3f6e131299a4b829ca77fc731ea5969c95a462
guozhinuan <guozhinuan@bigo.sg>
2024-01-26 10:15:41 +0100

    add ingress controller

--- bill-helmfile/install.sh
+++ bill-helmfile/install.sh
@@ -1,4 +1,5 @@
-helm install bill-cert charts/bill-cert-manager
+helm install bill-ingress-controller charts/bill-ingress-controller
+helm install bill-cert charts/bill-cert-manager --set leaderElection.namespace=default
 sleep 30
 helm install bill-issuers charts/bill-cert-issuer
 helm install bill-mysql charts/bill-mysql
