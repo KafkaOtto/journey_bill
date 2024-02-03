23c27660b11ea8db54d1d68878931a3433b246bd
guozhinuan <guozhinuan@bigo.sg>
2024-02-03 17:07:53 +0100

    helm uninstall

---
+++ bill-helmfile/uninstall.sh
@@ -0,0 +1,8 @@
+helm uninstall bill-nginx charts/bill-nginx
+helm uninstall bill-vue charts/bill-vue
+helm uninstall bill-spring charts/bill-spring
+helm uninstall bill-mysql charts/bill-mysql
+helm uninstall bill-ingress-controller charts/bill-ingress-controller
+helm uninstall bill-issuers charts/bill-cert-issuer
+helm uninstall bill-cert charts/bill-cert-manager
+
